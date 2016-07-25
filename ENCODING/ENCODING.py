429ff120c53daf62ece6978e0b485f5628041ecf
free-lunch <jjh783@gmail.com>
2016-07-25 14:31:04 +0900

    Add a solution "ENCODING" - It was passed and taked 28 ms in python2

--- ENCODING/ENCODING.py
+++ ENCODING/ENCODING.py
@@ -1,48 +1,56 @@
 import sys
 
+
 def solve(r, c, s):
+    bin_list = ""
+    for char in s:
+        if char == " ":
+            bin_list += "00000"
+        else:
+            bin_list += "{:05b}".format(ord(char)-64)
+
     pos = 0
-    str_list = []
     direction = 0
     dir_list = [1, c,-1, -c]
-
-    for i in xrange(r*c/5):
-        one_char = 0
-        for j in xrange(4,-1,-1):
-            if s[pos] == "1":
-                one_char += (1<<j)
-            s[pos] = -1
+    ret = [-1]*(r*c)
+    try:
+        for i in xrange(len(bin_list)):
+            ret[pos] = bin_list[i]
             pos += dir_list[direction]
             need_change_dir = False
 
             # Case : Visit string at matrix outside
             if direction == 0 and pos == c:
                 need_change_dir = True
-            elif direction == 1 and pos == len(s)-1+c:
+            elif direction == 1 and pos >= r*c:
                 need_change_dir = True
             # Case : Visit already visted string
-            elif s[pos] == -1:
+            elif ret[pos] != -1:
                 need_change_dir = True
 
             if need_change_dir:
                 pos -= dir_list[direction]
                 direction = (direction+1)%4
                 pos += dir_list[direction]
+    except:
+        return "0"
 
-        str_list.append(one_char)
+    for i in xrange(len(ret)):
+        if ret[i] == -1:
+            ret[i] = "0"
 
-    ret = ""
-    for c in str_list:
-        if c == 0:
-            ret += " "
-        else:
-            ret += chr(64+c)
-
-    return ret
+    return "".join(ret)
 
 if __name__ == "__main__":
-    rl = lambda : sys.stdin.readline().rstrip(' \t\r\n\0')
+    rl = lambda : sys.stdin.readline().rstrip('\t\r\n\0')
     for i in xrange(int(rl())):
-        input = rl().split()
-        r, c = int(input[0]), int(input[1])
-        print i+1, solve(r, c, list(input[2]))
+        input = rl()
+        # for i in xrange(len(input)):
+        try:
+            r, c, s = input.split(' ', 2)
+        except:
+            r, c = input.split(' ', 2)
+            s = ""
+
+        r, c = int(r), int(c)
+        print i+1, solve(r, c, s)
